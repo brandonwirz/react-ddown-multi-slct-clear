f5cdb7ea405af9314232940755b743dd14ce74aa
brandon wirz <bwirz75@gmail.com>
2023-05-19 15:20:27 -0600

    multi-select-clearable

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Select from "react-select";
+// import makeAnimated from "react-select/animated";
+import { colorOptions } from "./data";
+import "./styles.css";
+
+export default function AnimatedMulti() {
+  return (
+    <div className="dropdown">
+      <div className="content">
+        <Select
+          closeMenuOnSelect={false}
+          defaultValue={[colorOptions[4], colorOptions[5]]}
+          isMulti
+          options={colorOptions}
+        />
+      </div>
+    </div>
+  );
+}
